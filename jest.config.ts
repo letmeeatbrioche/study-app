44cc819ea91a5a77cb704992d460fb777cf68a5a
Haillie W <hailliewilloughby@gmail.com>
2024-01-07 13:38:21 -0600

    Add MongoDB bridge

--- jest.config.ts
+++ jest.config.ts
@@ -9,9 +9,10 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'babel',
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  "preset": "@shelf/jest-mongodb"
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
