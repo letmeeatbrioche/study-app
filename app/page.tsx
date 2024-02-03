034175f4d0d1de75f0e89e0a32b03bb0688a84d3
Haillie W <hailliewilloughby@gmail.com>
2024-02-03 14:10:56 -0600

    Add thumbnail component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Thumbnail from '@/components/Thumbnail'
+
+const testProps = {
+  name: 'Math and raggle and fraggle',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/EasternGraySquirrel_GAm.jpg'
+}
 
 export default function Home() {
   return (
-    <h1>HELLO</h1>
+    <>
+      <Thumbnail name={testProps.name} image={testProps.image}/>
+    </>
   )
 }
