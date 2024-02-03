fea6f0fe072fe73df156ad8b1cb6618402c782ed
Haillie W <hailliewilloughby@gmail.com>
2024-02-03 14:26:37 -0600

    Add delete category button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Thumbnail from '@/components/Thumbnail'
+import DeleteCategory from '@/components/DeleteCategory'
 
 const testProps = {
   name: 'Math and raggle and fraggle',
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <>
       <Thumbnail name={testProps.name} image={testProps.image}/>
+      <DeleteCategory />
     </>
   )
 }
