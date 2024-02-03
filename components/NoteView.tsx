ed7b90bff96301ed0c1ba8cf7e0a0e18420b6f95
Haillie W <hailliewilloughby@gmail.com>
2024-02-03 15:04:22 -0600

    Add view note component

---
+++ components/NoteView.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+type Props = {
+  image: string,
+  text: string,
+  title: string,
+  category: string
+}
+
+// GET title, image, category, text from note by id
+
+const NoteView = (props: Props) => {
+  return (
+    <>
+      <h1>Note View</h1>
+      <div className='note-view'>
+        <h2 className='note-title'>{props.title}</h2>
+        <h3 className='note-category'>{props.category}</h3>
+        <div className='note-image' style={{background: `url(${props.image})`}}></div>
+        <p>{props.text}</p>
+        <Link href={'#'}>
+          <button type='button'>Edit</button>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default NoteView
