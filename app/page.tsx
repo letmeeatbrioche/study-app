3f5ab642b0944f96fa340ac6a6b1e60b8d1a6831
Haillie W <hailliewilloughby@gmail.com>
2024-02-03 15:03:24 -0600

    Add props and NoteView comp

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Thumbnail from '@/components/Thumbnail'
 import DeleteCategory from '@/components/DeleteCategory'
+import NoteView from '@/components/NoteView'
 
 const testProps = {
   name: 'Math and raggle and fraggle',
-  image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/EasternGraySquirrel_GAm.jpg'
+  image: 'https://s3-eu-west-1.amazonaws.com/media.squirrelaccord.uk/2021/09/AdobeStock_239716417_Isle_of_Wight_square_256.jpg',
+  text: `"They use a mental, spatial map in order to find it. Think of a squirrel GPS. And they go off of, okay there's a rock here, and a tree there, the nut is somewhere between there," Tekiela said. Then, they use their sense of smell to find the exact location, even under the snow."`,
+  title: 'How squirrels find their burried food',
+  category: 'Squirrels'
 }
 
 export default function Home() {
@@ -13,6 +17,7 @@ export default function Home() {
     <>
       <Thumbnail name={testProps.name} image={testProps.image}/>
       <DeleteCategory />
+      <NoteView image={testProps.image} text={testProps.text} title={testProps.title} category={testProps.category} />
     </>
   )
 }
