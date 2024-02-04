a9418e815d6fdcc5fb5fab99ff2b8c4d01c0a6e9
Haillie W <hailliewilloughby@gmail.com>
2024-02-04 12:19:15 -0600

    Create delete note button comp.

---
+++ components/DeleteNoteButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const DeleteNoteButton = (props: Props) => {
+  // Function: delete note card from database and category
+  return (
+    <button type='button'>DeleteNoteButton</button>
+  )
+}
+
+export default DeleteNoteButton
