50bcca488f31d328639593880294bfa70db60003
Haillie W <hailliewilloughby@gmail.com>
2024-02-04 12:15:47 -0600

    Add discard edit button comp

---
+++ components/DiscardEditButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const DiscardEditButton = (props: Props) => {
+  // Function: redirect to previous page without performing any CRUD operations
+  return (
+    <button type='button'>DiscardEditButton</button>
+  )
+}
+
+export default DiscardEditButton
