ca8b7ed2cf0b26bab1c6109dc5702eb006f4473c
Haillie W <hailliewilloughby@gmail.com>
2024-02-05 12:51:25 -0600

    Add ImageUpload comp.

--- components/NoteEditCreate.tsx
+++ components/NoteEditCreate.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import SaveNoteButton from './SaveNoteButton'
 import DiscardEditButton from './DiscardEditButton'
 import CategoryDropdown from './CategoryDropdown'
+import ImageUpload from './ImageUpload'
 
 type Props = {}
 
 const NoteEditCreate = (props: Props) => {
   return (
     <div className='form'>
+      <h1>Note Edit/Create</h1>
       <DiscardEditButton />
       <form>
         <input type='text' placeholder='Title' />
         <CategoryDropdown />
-        <input type='image'></input>
+        <ImageUpload />
         <input type="text" placeholder='Notes...' />
         <SaveNoteButton />
       </form>
