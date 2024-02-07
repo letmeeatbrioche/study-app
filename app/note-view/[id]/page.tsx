6fb60c0efc346f0c0ccfea826aae7bc5c998c99b
Haillie W <hailliewilloughby@gmail.com>
2024-02-07 12:45:13 -0600

    Mod page

--- app/note-view/[id]/page.tsx
+++ app/note-view/[id]/page.tsx
@@ -1,3 +1,20 @@
-import NoteView from "@/components/NoteView";
+import NoteView from '@/components/NoteView'
+import React from 'react'
 
-<NoteView />
+const testProps = {
+  name: 'Math and raggle and fraggle',
+  image: 'https://s3-eu-west-1.amazonaws.com/media.squirrelaccord.uk/2021/09/AdobeStock_239716417_Isle_of_Wight_square_256.jpg',
+  text: `"They use a mental, spatial map in order to find it. Think of a squirrel GPS. And they go off of, okay there's a rock here, and a tree there, the nut is somewhere between there," Tekiela said. Then, they use their sense of smell to find the exact location, even under the snow."`,
+  title: 'How squirrels find their burried food',
+  category: 'Squirrels'
+}
+
+type Props = {}
+
+const NoteViewPage = (props: Props) => {
+  return (
+    <NoteView image={testProps.image} text={testProps.text} title={testProps.title} category={testProps.category} />
+  )
+}
+
+export default NoteViewPage
