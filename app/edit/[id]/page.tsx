5c1ca57869deb1f8c2e63969036c899c7210616f
Haillie W <hailliewilloughby@gmail.com>
2024-02-07 14:09:29 -0600

    Create edit page

---
+++ app/edit/[id]/page.tsx
@@ -0,0 +1,15 @@
+import NoteEditCreate from '@/components/NoteEditCreate'
+import { Paper } from '@mui/material'
+import React from 'react'
+
+type Props = {}
+
+const editNote = (props: Props) => {
+  return (
+    <>
+      <NoteEditCreate />
+    </>
+  )
+}
+
+export default editNote
