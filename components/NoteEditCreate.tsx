2949b8ea6af7eb197ee3d6837307c5f2bfa928f6
Haillie W <hailliewilloughby@gmail.com>
2024-02-07 17:10:23 -0600

    Change comp. to work w/ props

--- components/NoteEditCreate.tsx
+++ components/NoteEditCreate.tsx
@@ -18,15 +18,22 @@ const NoteEditCreate = (props: Props) => {
   return (
     <Paper className='paper-form' elevation={3}>
       <h1>Note Edit/Create</h1>
-      <DiscardEditButton />
+      <h2>{props.title}</h2>
       <form>
-        <input type='text' placeholder='Title' />
-        <CategoryDropdown />
-        <ImageUpload />
-        <input type="text" placeholder='Notes...' />
-        <SaveNoteButton />
+        <Grid container justifyContent='space-around' >
+          <Grid item xs={5.7} className='form-grid-item'>
+            <input className='note-title-input' type='text' placeholder='Title' defaultValue={props.title} />
+            <ImageUpload image={props.image} /> {/** GET IMAGE FROM PROPS TO DISPLAY */}
+          </Grid>
+          <Grid item xs={5.7} className='form-grid-item'>
+            <CategoryDropdown currentCategory={props.category} categories={props.categories} />
+            <textarea className='note-text-input' placeholder='Notes...' defaultValue={props.text} />
+            <SaveNoteButton />
+            <DiscardEditButton />
+          </Grid>
+        </Grid>
       </form>
-    </div>
+    </Paper>
   )
 }
 
