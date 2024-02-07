5492a9c20b4e6b0557e2d3429ca971e34a910b03
Haillie W <hailliewilloughby@gmail.com>
2024-02-07 14:07:28 -0600

    Change structure & convert to mui

--- components/NoteEditCreate.tsx
+++ components/NoteEditCreate.tsx
@@ -3,23 +3,62 @@ import SaveNoteButton from './SaveNoteButton'
 import DiscardEditButton from './DiscardEditButton'
 import CategoryDropdown from './CategoryDropdown'
 import ImageUpload from './ImageUpload'
+import { Grid, Paper } from '@mui/material'
 
 type Props = {}
 
 const NoteEditCreate = (props: Props) => {
   return (
-    <div className='form'>
+    <Paper className='paper-form' elevation={3}>
       <h1>Note Edit/Create</h1>
-      <DiscardEditButton />
       <form>
-        <input type='text' placeholder='Title' />
-        <CategoryDropdown />
-        <ImageUpload />
-        <input type="text" placeholder='Notes...' />
-        <SaveNoteButton />
+        <Grid container justifyContent='space-around' >
+          <Grid item xs={5.7} className='form-grid-item'>
+            <input className='note-title-input' type='text' placeholder='Title' />
+            <ImageUpload />
+          </Grid>
+          <Grid item xs={5.7} className='form-grid-item'>
+            <CategoryDropdown />
+            <textarea className='note-text-input' placeholder='Notes...' />
+            <SaveNoteButton />
+            <DiscardEditButton />
+          </Grid>
+        </Grid>
       </form>
-    </div>
+    </Paper>
+
   )
 }
+
+{/* <Box sx={{ width: '100%' }}>
+          <h1>Note View</h1>
+          <Grid container >
+            <Grid xs={6}>
+              <div className='grid-item' >
+                <h2 className='note-title'>{props.title}</h2>
+                <div className='note-image' style={{background: `url(${props.image})`}}></div>
+              </div>
+            </Grid>
+            <Grid xs={6}>
+              <div className='grid-item' >
+                <h3 className='note-category'>{props.category}</h3>
+                <p>{props.text}</p>
+                <Link href={'/edit/123'}>
+                  <Button variant="contained" href='/edit/123'>
+                    Edit
+                  </Button>
+                </Link>
+                {!params &&
+                  <Link href={'/note-view/123'}>
+                    <Button variant="contained" href="/note-view/123">
+                      View Note
+                    </Button>
+                  </Link>
+                }
+              </div>
+            </Grid>
+          </Grid>
+        </Box> */}
+
 
 export default NoteEditCreate
