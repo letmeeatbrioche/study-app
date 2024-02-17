dc5601ba2c8866eb997af25895084c9d6fa8bc0e
Haillie W <hailliewilloughby@gmail.com>
2024-02-16 18:12:24 -0600

    Add get all notes request

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,3 +1,5 @@
+import { NextRequest, NextResponse } from "next/server";
+
 // GET all categories
 export const getCategories = async () => {
   try {
@@ -12,5 +14,22 @@ export const getCategories = async () => {
     return categories;
   } catch (error) {
     console.log('Error in getCategories:', error);
+  }
+}
+
+// GET all notes
+export const getNotes = async (id: string) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/${id}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      cache: 'no-store'
+    });
+    if (!res.ok) throw new Error('Could not get notes in getNotes');
+    const notes = await res.json();
+    return notes;
+  } catch (error) {
+    console.log('Error in getNotes:', error);
   }
 }
