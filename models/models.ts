a36ed80e54c86f8e9f1f7665eeed1ea7ffcbe671
Haillie W <hailliewilloughby@gmail.com>
2024-02-16 18:13:31 -0600

    Modify note class

--- models/models.ts
+++ models/models.ts
@@ -3,10 +3,10 @@ import { ObjectId } from "mongodb";
 // For documents in the "notes" collection
 export class Note {
   constructor(
-    public title: string,
     public text: string,
-    public image: string,
-    public category_id: ObjectId,
+    public title?: string,
+    public image?: string,
+    public category?: ObjectId,
     public _id?: ObjectId,
   ){}
 }
