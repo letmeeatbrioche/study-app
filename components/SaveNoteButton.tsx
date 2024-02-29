f97d4dd696d57fd205bd93c3d1143abd3471e270
Haillie W <hailliewilloughby@gmail.com>
2024-02-29 13:52:49 -0600

    Add button as sumbit type

--- components/SaveNoteButton.tsx
+++ components/SaveNoteButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@mui/material'
 import React from 'react'
 
 type Props = {
-  id?: string
+  id?: string,
 }
 
 const SaveNoteButton = (props: Props) => {
@@ -10,7 +10,7 @@ const SaveNoteButton = (props: Props) => {
     // Functions:
       // Change submit button behavior depending on if creating or editing a note
       // Editing a note will require (I think) a note id prop
-    <Button variant="contained">
+    <Button variant="contained" type='submit'>
       Save
     </Button>
   )
