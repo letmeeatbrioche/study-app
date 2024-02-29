4d49e89a7a8739c1fe744cccd81adee899ad20ca
Haillie W <hailliewilloughby@gmail.com>
2024-02-29 13:55:54 -0600

    Change category name

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 // Function to get all categories
 const getAllCategories = () => {
-  let categories = ['Category 1', 'Category 2', 'Category 3', 'Squirrels'];
+  let categories = ['Category 1', 'Category 2', '65cd6fa735525e420e529d46', 'Squirrels'];
   return categories;
 }
 
