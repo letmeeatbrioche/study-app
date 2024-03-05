e41b379bff8448e24bc7fb5ab7c266e33b32700b
Haillie W <hailliewilloughby@gmail.com>
2024-03-05 14:01:48 -0600

    Add category creation functionality

--- components/NoteCreate.tsx
+++ components/NoteCreate.tsx
@@ -66,8 +66,12 @@ const NoteCreate = (props: Props) => {
     var data = new FormData(event.target);
     console.log('data:', data);
     var formObject = Object.fromEntries(data.entries());
-    const categoryId = props.categories.find((element) => element.name === selectedCategory);
-    formObject.category = categoryId._id;
+    if (categoryNames.indexOf(selectedCategory) > -1) {
+      const categoryId = props.categories.find((element) => element.name === selectedCategory);
+      formObject.category = categoryId._id;
+    } else {
+      formObject.categoryName = selectedCategory;
+    }
     formObject.image = uploadedImage;
     setNoteData(formObject);
     console.log(formObject);
