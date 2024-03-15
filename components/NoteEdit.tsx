a8dba1636d61ec91c15ad2822c0522b1bd65e057
Haillie W <hailliewilloughby@gmail.com>
2024-03-14 19:10:37 -0500

    Add navigation check

--- components/NoteEdit.tsx
+++ components/NoteEdit.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SaveNoteButton from './SaveNoteButton'
 import DiscardEditButton from './DiscardEditButton'
 import CategoryDropdown from './CategoryDropdown'
@@ -61,6 +61,50 @@ const NoteEdit = (props: Props) => {
     </>
   ) : null
 
+
+  // Handle Navigation with Unsaved Changes
+  const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
+
+  const handleChange = (e, input) => {
+    console.log('in handleChange');
+    if (input === 'title') setNoteTitle(e.target.value);
+    if (input === 'text') setNoteText(e.target.value);
+    setHasUnsavedChanges(true);
+  };
+
+  useEffect(() => {
+    console.log('uploadedImage:', uploadedImage);
+
+    setHasUnsavedChanges(true);
+
+    const handleBeforeUnload = (e) => {
+      console.log('handlingBeforeUnload');
+      if (hasUnsavedChanges) {
+        console.log('hasUnsavedChanges');
+        e.preventDefault();
+        e.returnValue = '';
+      }
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [hasUnsavedChanges, uploadedImage, selectedCategory]);
+
+  const handleNavigation = (url) => {
+    if (hasUnsavedChanges) {
+      const confirmLeave = window.confirm('You have unsaved changes! Are you sure you want to leave?');
+      if (!confirmLeave) {
+        return;
+      }
+      setHasUnsavedChanges(false);
+    }
+    router.push(url);
+  };
+
+
   // EDIT NOTE FORM
   const formSubmit = (event) => {
     event.preventDefault();
@@ -101,7 +145,7 @@ const NoteEdit = (props: Props) => {
       <form onSubmit={formSubmit}>
         <Grid container justifyContent='space-around' >
           <Grid item xs={5.7} className='form-grid-item'>
-            <input className='note-title-input' name='title' type='text' placeholder='Title' value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)} />
+            <input className='note-title-input' name='title' type='text' placeholder='Title' value={noteTitle} onChange={(e) => handleChange(e, 'title')} />
 
         <div className='image-uploader'>
           <div>
@@ -143,7 +187,7 @@ const NoteEdit = (props: Props) => {
           </Grid>
           <Grid item xs={5.7} className='form-grid-item'>
             <CategoryDropdown categories={categoryNames} isActive={isActive} setIsActive={setIsActive} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-            <textarea className='note-text-input' name='text' placeholder='Notes...' value={noteText} onChange={(e) => setNoteText(e.target.value)} />
+            <textarea className='note-text-input' name='text' placeholder='Notes...' value={noteText} onChange={(e) => handleChange(e, 'text')} />
             <SaveNoteButton />
             <DiscardEditButton buttonText='Note' confirmationText='note' />
           </Grid>
@@ -154,80 +198,3 @@ const NoteEdit = (props: Props) => {
 }
 
 export default NoteEdit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use client"
-// import React, { useState } from 'react'
-// import SaveNoteButton from './SaveNoteButton'
-// import DiscardEditButton from './DiscardEditButton'
-// import CategoryDropdown from './CategoryDropdown'
-// import ImageUpload from './ImageUpload'
-// import { Grid, Paper } from '@mui/material'
-
-// type Props = {
-//   image?: string,
-//   text: string,
-//   title?: string,
-//   category?: string,
-//   categories?: string[],
-//   id: string
-// }
-
-// const NoteEdit = (props: Props) => {
-//   const [noteTitle, setNoteTitle] = useState(props.title);
-//   const [noteText, setNoteText] = useState(props.text);
-//   const [isActive, setIsActive] = useState(false);
-//   const [selectedCategory, setSelectedCategory] = useState(props.category || props.categories[0]);
-
-//   return (
-//     <Paper className='paper-form' elevation={3}>
-//       <h1>Note Edit</h1>
-//       <form>
-//         <Grid container justifyContent='space-around' >
-//           <Grid item xs={5.7} className='form-grid-item'>
-//             <input className='note-title-input' type='text' placeholder='Title' value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)} />
-//             <ImageUpload image={props.image} /> {/** GET IMAGE FROM PROPS TO DISPLAY */}
-//           </Grid>
-//           <Grid item xs={5.7} className='form-grid-item'>
-//             <CategoryDropdown categories={props.categories} isActive={isActive} setIsActive={setIsActive} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-//             <textarea className='note-text-input' placeholder='Notes...' value={noteText} onChange={(e) => setNoteText(e.target.value)} />
-//             <SaveNoteButton />
-//             <DiscardEditButton buttonText='Edits' confirmationText='edits' />
-//           </Grid>
-//         </Grid>
-//       </form>
-//     </Paper>
-//   )
-// }
-
-// export default NoteEdit
