bcad5dcc553af4cd119ade9b59240c8037738b6e
Haillie W <hailliewilloughby@gmail.com>
2024-03-14 19:12:56 -0500

    Comment unused code

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,18 +3,17 @@ import { getCategories } from '@/utils/requests';
 import React from 'react'
 
 // Function to get all categories
-const getAllCategories = () => {
-  let categories = ['Category 1', 'Category 2', '65cd6fa735525e420e529d46', 'Squirrels'];
-  return categories;
-}
-
+// const getAllCategories = () => {
+//   let categories = ['Category 1', 'Category 2', '65cd6fa735525e420e529d46', 'Squirrels'];
+//   return categories;
+// }
 
 type Props = {}
 
 const CreateNote = async (props: Props) => {
   const gottenCategories = await getCategories();
-  const categories = await getAllCategories();
-  console.log('after gettingCategories in CreateNote/page.tsx:', gottenCategories);
+  // const categories = await getAllCategories();
+  // console.log('after gettingCategories in CreateNote/page.tsx:', gottenCategories);
   return (
     <NoteCreate categories={gottenCategories} />
   )
